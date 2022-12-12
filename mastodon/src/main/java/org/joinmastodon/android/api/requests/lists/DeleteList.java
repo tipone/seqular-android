a3267f6cd3c10f7c1834f012713326233295022d
LucasGGamerM <lucassggabriel@gmail.com>
2022-12-12 16:10:27 -0300

    The placeholders are better. But the api thing isnt working

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/lists/DeleteList.java
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/lists/DeleteList.java
@@ -1,10 +1,9 @@
 package org.joinmastodon.android.api.requests.lists;
 
 import org.joinmastodon.android.api.MastodonAPIRequest;
-import java.util.List;
 
-public class RemoveList extends MastodonAPIRequest<Object> {
-    public RemoveList(String listId){
+public class DeleteList extends MastodonAPIRequest<Object> {
+    public DeleteList(String listId){
         super(HttpMethod.DELETE, "/lists/"+listId, Object.class);
     }
 }
