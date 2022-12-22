61049a1302974aa9e202d8c6f50d2ae21639b3ac
LucasGGamerM <lucassggabriel@gmail.com>
2022-12-22 19:27:02 -0300

    Adding the nord theme

--- mastodon/src/main/java/org/joinmastodon/android/fragments/SettingsFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/SettingsFragment.java
@@ -18,6 +18,8 @@ import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
@@ -102,7 +104,25 @@ public class SettingsFragment extends MastodonToolbarFragment{
 			GlobalUserPreferences.disableMarquee=i.checked;
 			GlobalUserPreferences.save();
 		}));
-		items.add(new ColorPicker());
+		items.add(new ButtonItem(R.string.sk_settings_color_palette, R.drawable.ic_fluent_color_24_regular, b->{
+			PopupMenu popupMenu=new PopupMenu(getActivity(), b, Gravity.CENTER_HORIZONTAL);
+			popupMenu.inflate(R.menu.color_palettes);
+			popupMenu.getMenu().findItem(R.id.m3_color).setVisible(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S);
+			popupMenu.setOnMenuItemClickListener(SettingsFragment.this::onColorPreferenceClick);
+			b.setOnTouchListener(popupMenu.getDragToOpenListener());
+			b.setOnClickListener(v->popupMenu.show());
+			b.setText(switch(GlobalUserPreferences.color){
+				case MATERIAL3 -> R.string.sk_color_palette_material3;
+				case PINK -> R.string.sk_color_palette_pink;
+				case PURPLE -> R.string.sk_color_palette_purple;
+				case GREEN -> R.string.sk_color_palette_green;
+				case BLUE -> R.string.sk_color_palette_blue;
+				case BROWN -> R.string.sk_color_palette_brown;
+				case RED -> R.string.sk_color_palette_red;
+				case YELLOW -> R.string.sk_color_palette_yellow;
+				case NORD -> R.string.sk_color_palette_nord;
+			});
+		}));
 
 		items.add(new HeaderItem(R.string.settings_behavior));
 		items.add(new SwitchItem(R.string.settings_gif, R.drawable.ic_fluent_gif_24_regular, GlobalUserPreferences.playGifs, i->{
@@ -248,12 +268,28 @@ public class SettingsFragment extends MastodonToolbarFragment{
 		restartActivityToApplyNewTheme();
 	}
 
-	private void onColorPreferenceClick(GlobalUserPreferences.ColorPreference color){
+	private boolean onColorPreferenceClick(MenuItem item){
+		ColorPreference pref = null;
+		int id = item.getItemId();
 
-		GlobalUserPreferences.color=color;
+		if (id == R.id.m3_color) pref = ColorPreference.MATERIAL3;
+		else if (id == R.id.pink_color) pref = ColorPreference.PINK;
+		else if (id == R.id.purple_color) pref = ColorPreference.PURPLE;
+		else if (id == R.id.green_color) pref = ColorPreference.GREEN;
+		else if (id == R.id.blue_color) pref = ColorPreference.BLUE;
+		else if (id == R.id.brown_color) pref = ColorPreference.BROWN;
+		else if (id == R.id.red_color) pref = ColorPreference.RED;
+		else if (id == R.id.yellow_color) pref = ColorPreference.YELLOW;
+		else if (id == R.id.nord_color) pref = ColorPreference.NORD;
+
+		if (pref == null) return false;
+
+		GlobalUserPreferences.color=pref;
 		GlobalUserPreferences.save();
 		restartActivityToApplyNewTheme();
+		return true;
 	}
+
 
 	private void onTrueBlackThemeChanged(SwitchItem item){
 		GlobalUserPreferences.trueBlackTheme=item.checked;
@@ -458,6 +494,23 @@ public class SettingsFragment extends MastodonToolbarFragment{
 		}
 	}
 
+	public class ButtonItem extends Item{
+		private int text;
+		private int icon;
+		private Consumer<Button> buttonConsumer;
+
+		public ButtonItem(@StringRes int text, @DrawableRes int icon, Consumer<Button> buttonConsumer) {
+			this.text = text;
+			this.icon = icon;
+			this.buttonConsumer = buttonConsumer;
+		}
+
+		@Override
+		public int getViewType(){
+			return 8;
+		}
+	}
+
 	public class ColorPicker extends Item{
 		@Override
 		public int getViewType(){
@@ -549,7 +602,7 @@ public class SettingsFragment extends MastodonToolbarFragment{
 				case 5 -> new HeaderViewHolder(true);
 				case 6 -> new FooterViewHolder();
 				case 7 -> new UpdateViewHolder();
-				case 8 -> new ColorPickerViewHolder();
+				case 8 -> new ButtonViewHolder();
 				default -> throw new IllegalStateException("Unexpected value: "+viewType);
 			};
 		}
@@ -678,78 +731,25 @@ public class SettingsFragment extends MastodonToolbarFragment{
 			}
 		}
 	}
-	private class ColorPickerViewHolder extends BindableViewHolder<ColorPicker>{
+
+	private class ButtonViewHolder extends BindableViewHolder<ButtonItem>{
 		private final Button button;
-		private final PopupMenu popupMenu;
 		private final ImageView icon;
+		private final TextView text;
 
 		@SuppressLint("ClickableViewAccessibility")
-		public ColorPickerViewHolder(){
-			super(getActivity(), R.layout.item_settings_color_picker, list);
+		public ButtonViewHolder(){
+			super(getActivity(), R.layout.item_settings_button, list);
+			text=findViewById(R.id.text);
 			icon=findViewById(R.id.icon);
-			button=findViewById(R.id.color_picker_button);
-			popupMenu=new PopupMenu(getActivity(), button, Gravity.CENTER_HORIZONTAL);
-			popupMenu.inflate(R.menu.color_picker);
-			popupMenu.setOnMenuItemClickListener(item->{
-				GlobalUserPreferences.ColorPreference pref;
-				int id=item.getItemId();
-				if(id==R.id.pink_color) {
-					pref = GlobalUserPreferences.ColorPreference.PINK;
-					onColorPreferenceClick(pref);
-				}
-				else if(id==R.id.purple_color) {
-					pref = GlobalUserPreferences.ColorPreference.PURPLE;
-					onColorPreferenceClick(pref);
-				}
-				else if(id==R.id.green_color) {
-					pref = GlobalUserPreferences.ColorPreference.GREEN;
-					onColorPreferenceClick(pref);
-				}
-				else if(id==R.id.blue_color) {
-					pref = GlobalUserPreferences.ColorPreference.BLUE;
-					onColorPreferenceClick(pref);
-				}
-				else if(id==R.id.orange_color) {
-					pref = ColorPreference.BROWN;
-					onColorPreferenceClick(pref);
-				}
-				else if(id==R.id.yellow_color) {
-					pref = GlobalUserPreferences.ColorPreference.YELLOW;
-					onColorPreferenceClick(pref);
-				}
-				else if(id==R.id.red_color) {
-					pref = GlobalUserPreferences.ColorPreference.RED;
-					onColorPreferenceClick(pref);
-				}
-				else if(id==R.id.m3_color) {
-					if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-						pref = GlobalUserPreferences.ColorPreference.MATERIAL3;
-						onColorPreferenceClick(pref);
-					}
-				}
-				else
-					return false;
-				return true;
-			});
-//			UiUtils.enablePopupMenuIcons(getActivity(), popupMenu);
-			popupMenu.getMenu().findItem(R.id.m3_color).setVisible(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S);
-			button.setOnTouchListener(popupMenu.getDragToOpenListener());
-			button.setOnClickListener(v->popupMenu.show());
+			button=findViewById(R.id.button);
 		}
 
 		@Override
-		public void onBind(ColorPicker item){
-			icon.setImageResource(R.drawable.ic_fluent_color_24_regular);
-			button.setText(switch(GlobalUserPreferences.color){
-				case PINK -> R.string.sk_color_palette_pink;
-				case PURPLE -> R.string.sk_color_palette_purple;
-				case GREEN -> R.string.sk_color_palette_green;
-				case BLUE -> R.string.sk_color_palette_blue;
-				case BROWN -> R.string.sk_color_palette_brown;
-				case YELLOW -> R.string.sk_color_palette_yellow;
-				case RED -> R.string.sk_color_palette_red;
-				case MATERIAL3 -> R.string.sk_color_palette_material3;
-			});
+		public void onBind(ButtonItem item){
+			text.setText(item.text);
+			icon.setImageResource(item.icon);
+			item.buttonConsumer.accept(button);
 		}
 	}
 
