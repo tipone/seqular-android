78d0add808b812cfe20513be96e9e7724314b046
sk22 <sk22@mailbox.org>
2023-01-13 00:35:48 -0300

    New home layout with public timelines (#288)
    * add dummy popup menu
    * add pager to home fragment
    * reduce pager sensitivity
    * remove timelines from discover fragment
    * add fabs to timelines
    * change info banner color
    * add back toolbar functionality
    * update icons on navigate
    * handle back press
    * add lists and hashtags
    * use tabs
    * improve timeline title
    * tweak switcher behavior
    * fix show new posts button appearance
    * hide show new posts button on reload
    * tweak show new posts animations
    * work around crash theme switch
    * enable disabling federated timeline

--- mastodon/src/main/java/org/joinmastodon/android/ui/utils/UiUtils.java
+++ mastodon/src/main/java/org/joinmastodon/android/ui/utils/UiUtils.java
@@ -31,6 +31,7 @@ import android.provider.OpenableColumns;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.HapticFeedbackConstants;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -111,6 +112,8 @@ import androidx.annotation.StringRes;
 import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
 import me.grishka.appkit.Nav;
 import me.grishka.appkit.api.Callback;
 import me.grishka.appkit.api.ErrorResponse;
@@ -1058,5 +1061,20 @@ public class UiUtils{
 			case PRIVATE -> R.string.visibility_followers_only;
 			case DIRECT -> R.string.visibility_private;
 		});
+	}
+
+	// https://github.com/tuskyapp/Tusky/pull/3148
+	public static void reduceSwipeSensitivity(ViewPager2 pager) {
+		try {
+			Field recyclerViewField = ViewPager2.class.getDeclaredField("mRecyclerView");
+			recyclerViewField.setAccessible(true);
+			RecyclerView recyclerView = (RecyclerView) recyclerViewField.get(pager);
+			Field touchSlopField = RecyclerView.class.getDeclaredField("mTouchSlop");
+			touchSlopField.setAccessible(true);
+			int touchSlop = touchSlopField.getInt(recyclerView);
+			touchSlopField.set(recyclerView, touchSlop * 3);
+		} catch (Exception ex) {
+			Log.e("reduceSwipeSensitivity", Log.getStackTraceString(ex));
+		}
 	}
 }
