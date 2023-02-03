72f638c96cff1ff39667c18ce4cebaecea11b943
sk <sk22@mailbox.org>
2023-02-03 14:50:06 +0100

    move metadata to profile

--- mastodon/src/main/java/org/joinmastodon/android/fragments/ProfileFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/ProfileFragment.java
@@ -1,26 +1,21 @@
 package org.joinmastodon.android.fragments;
 
-import static android.content.Context.CLIPBOARD_SERVICE;
-
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.app.Fragment;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Outline;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
 import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
 import android.text.style.ImageSpan;
@@ -38,11 +33,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.Toolbar;
 
 import org.joinmastodon.android.GlobalUserPreferences;
@@ -61,6 +54,7 @@ import org.joinmastodon.android.model.Account;
 import org.joinmastodon.android.model.AccountField;
 import org.joinmastodon.android.model.Attachment;
 import org.joinmastodon.android.model.Relationship;
+import org.joinmastodon.android.ui.BetterItemAnimator;
 import org.joinmastodon.android.ui.SimpleViewHolder;
 import org.joinmastodon.android.ui.SingleImagePhotoViewerListener;
 import org.joinmastodon.android.ui.drawables.CoverOverlayGradientDrawable;
@@ -69,8 +63,10 @@ import org.joinmastodon.android.ui.tabs.TabLayout;
 import org.joinmastodon.android.ui.tabs.TabLayoutMediator;
 import org.joinmastodon.android.ui.text.CustomEmojiSpan;
 import org.joinmastodon.android.ui.text.HtmlParser;
+import org.joinmastodon.android.ui.utils.SimpleTextWatcher;
 import org.joinmastodon.android.ui.utils.UiUtils;
 import org.joinmastodon.android.ui.views.CoverImageView;
+import org.joinmastodon.android.ui.views.LinkedTextView;
 import org.joinmastodon.android.ui.views.NestedRecyclerScrollView;
 import org.joinmastodon.android.ui.views.ProgressBarButton;
 import org.parceler.Parcels;
@@ -84,6 +80,9 @@ import java.util.Collections;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import androidx.viewpager2.widget.ViewPager2;
@@ -94,10 +93,17 @@ import me.grishka.appkit.api.SimpleCallback;
 import me.grishka.appkit.fragments.BaseRecyclerFragment;
 import me.grishka.appkit.fragments.LoaderFragment;
 import me.grishka.appkit.fragments.OnBackPressedListener;
+import me.grishka.appkit.imageloader.ImageLoaderRecyclerAdapter;
+import me.grishka.appkit.imageloader.ImageLoaderViewHolder;
+import me.grishka.appkit.imageloader.ListImageLoaderWrapper;
+import me.grishka.appkit.imageloader.RecyclerViewDelegate;
 import me.grishka.appkit.imageloader.ViewImageLoader;
+import me.grishka.appkit.imageloader.requests.ImageLoaderRequest;
 import me.grishka.appkit.imageloader.requests.UrlImageLoaderRequest;
+import me.grishka.appkit.utils.BindableViewHolder;
 import me.grishka.appkit.utils.CubicBezierInterpolator;
 import me.grishka.appkit.utils.V;
+import me.grishka.appkit.views.UsableRecyclerView;
 
 public class ProfileFragment extends LoaderFragment implements OnBackPressedListener, ScrollableToTop{
 	private static final int AVATAR_RESULT=722;
@@ -138,6 +144,16 @@ public class ProfileFragment extends LoaderFragment implements OnBackPressedList
 	private WindowInsets childInsets;
 	private PhotoViewer currentPhotoViewer;
 	private boolean editModeLoading;
+
+	private static final int MAX_FIELDS=4;
+
+	// from ProfileAboutFragment
+	public UsableRecyclerView list;
+	private List<AccountField> metadataListData=Collections.emptyList();
+	private MetadataAdapter adapter;
+	private ItemTouchHelper dragHelper=new ItemTouchHelper(new ReorderCallback());
+	private RecyclerView.ViewHolder draggedViewHolder;
+	private ListImageLoaderWrapper imgLoader;
 
 	public ProfileFragment(){
 		super(R.layout.loader_fragment_overlay_toolbar);
@@ -206,6 +222,7 @@ public class ProfileFragment extends LoaderFragment implements OnBackPressedList
 		notifyProgress=content.findViewById(R.id.notify_progress);
 		fab=content.findViewById(R.id.fab);
 		followsYouView=content.findViewById(R.id.follows_you);
+		list=content.findViewById(R.id.metadata);
 
 		avatar.setOutlineProvider(new ViewOutlineProvider(){
 			@Override
@@ -303,6 +320,14 @@ public class ProfileFragment extends LoaderFragment implements OnBackPressedList
 			return true;
 		});
 
+		// from ProfileAboutFragment
+		list.setItemAnimator(new BetterItemAnimator());
+		list.setDrawSelectorOnTop(true);
+		list.setLayoutManager(new LinearLayoutManager(getActivity()));
+		imgLoader=new ListImageLoaderWrapper(getActivity(), list, new RecyclerViewDelegate(list), null);
+		list.setAdapter(adapter=new MetadataAdapter());
+		list.setClipToPadding(false);
+
 		return sizeWrapper;
 	}
 
@@ -359,7 +384,7 @@ public class ProfileFragment extends LoaderFragment implements OnBackPressedList
 			pinnedPostsFragment=AccountTimelineFragment.newInstance(accountID, account, GetAccountStatuses.Filter.PINNED, false);
 			mediaFragment=AccountTimelineFragment.newInstance(accountID, account, GetAccountStatuses.Filter.MEDIA, false);
 			aboutFragment=new ProfileAboutFragment();
-			aboutFragment.setFields(fields);
+			setFields(fields);
 		}
 		pager.getAdapter().notifyDataSetChanged();
 		super.dataLoaded();
@@ -519,9 +544,7 @@ public class ProfileFragment extends LoaderFragment implements OnBackPressedList
 			fields.add(field);
 		}
 
-		if(aboutFragment!=null){
-			aboutFragment.setFields(fields);
-		}
+		setFields(fields);
 	}
 
 	private void updateToolbar(){
@@ -829,6 +852,10 @@ public class ProfileFragment extends LoaderFragment implements OnBackPressedList
 		set.start();
 
 		aboutFragment.enterEditMode(account.source.fields);
+
+		metadataListData=account.source.fields;
+		adapter.notifyDataSetChanged();
+		dragHelper.attachToRecyclerView(list);
 	}
 
 	private void exitEditMode(){
@@ -1047,6 +1074,230 @@ public class ProfileFragment extends LoaderFragment implements OnBackPressedList
 		@Override
 		public int getItemViewType(int position){
 			return position;
+		}
+	}
+
+	// from ProfileAboutFragment
+	public void setFields(ArrayList<AccountField> fields){
+		metadataListData=fields;
+		if (isInEditMode) {
+			isInEditMode=false;
+			dragHelper.attachToRecyclerView(null);
+		}
+		if (adapter != null) adapter.notifyDataSetChanged();
+		if (aboutFragment != null) aboutFragment.setFields(fields);
+	}
+
+	private class MetadataAdapter extends UsableRecyclerView.Adapter<BaseViewHolder> implements ImageLoaderRecyclerAdapter {
+		public MetadataAdapter(){
+			super(imgLoader);
+		}
+
+		@NonNull
+		@Override
+		public BaseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+			return switch(viewType){
+				case 0 -> new AboutViewHolder();
+				case 1 -> new EditableAboutViewHolder();
+				case 2 -> new AddRowViewHolder();
+				default -> throw new IllegalStateException("Unexpected value: "+viewType);
+			};
+		}
+
+		@Override
+		public void onBindViewHolder(BaseViewHolder holder, int position){
+			if(position<metadataListData.size()){
+				holder.bind(metadataListData.get(position));
+			}else{
+				holder.bind(null);
+			}
+			super.onBindViewHolder(holder, position);
+		}
+
+		@Override
+		public int getItemCount(){
+			if(isInEditMode){
+				int size=metadataListData.size();
+				if(size<MAX_FIELDS)
+					size++;
+				return size;
+			}
+			return metadataListData.size();
+		}
+
+		@Override
+		public int getItemViewType(int position){
+			if(isInEditMode){
+				return position==metadataListData.size() ? 2 : 1;
+			}
+			return 0;
+		}
+
+		@Override
+		public int getImageCountForItem(int position){
+			return isInEditMode || metadataListData.get(position).emojiRequests==null
+					? 0 : metadataListData.get(position).emojiRequests.size();
+		}
+
+		@Override
+		public ImageLoaderRequest getImageRequest(int position, int image){
+			return metadataListData.get(position).emojiRequests.get(image);
+		}
+	}
+
+	private abstract class BaseViewHolder extends BindableViewHolder<AccountField> {
+		public BaseViewHolder(int layout){
+			super(getActivity(), layout, list);
+		}
+	}
+
+	private class AboutViewHolder extends BaseViewHolder implements ImageLoaderViewHolder {
+		private TextView title;
+		private LinkedTextView value;
+
+		public AboutViewHolder(){
+			super(R.layout.item_profile_about);
+			title=findViewById(R.id.title);
+			value=findViewById(R.id.value);
+		}
+
+		@Override
+		public void onBind(AccountField item){
+			title.setText(item.parsedName);
+			value.setText(item.parsedValue);
+			if(item.verifiedAt!=null){
+				int textColor=UiUtils.isDarkTheme() ? 0xFF89bb9c : 0xFF5b8e63;
+				value.setTextColor(textColor);
+				value.setLinkTextColor(textColor);
+				Drawable check=getResources().getDrawable(R.drawable.ic_fluent_checkmark_24_regular, getActivity().getTheme()).mutate();
+				check.setTint(textColor);
+				value.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, check, null);
+			}else{
+				value.setTextColor(UiUtils.getThemeColor(getActivity(), android.R.attr.textColorPrimary));
+				value.setLinkTextColor(UiUtils.getThemeColor(getActivity(), android.R.attr.colorAccent));
+				value.setCompoundDrawables(null, null, null, null);
+			}
+		}
+
+		@Override
+		public void setImage(int index, Drawable image){
+			CustomEmojiSpan span=index>=item.nameEmojis.length ? item.valueEmojis[index-item.nameEmojis.length] : item.nameEmojis[index];
+			span.setDrawable(image);
+			title.invalidate();
+			value.invalidate();
+		}
+
+		@Override
+		public void clearImage(int index){
+			setImage(index, null);
+		}
+	}
+
+	private class EditableAboutViewHolder extends BaseViewHolder {
+		private EditText title;
+		private EditText value;
+
+		public EditableAboutViewHolder(){
+			super(R.layout.item_profile_about_editable);
+			title=findViewById(R.id.title);
+			value=findViewById(R.id.value);
+			findViewById(R.id.dragger_thingy).setOnLongClickListener(v->{
+				dragHelper.startDrag(this);
+				return true;
+			});
+			title.addTextChangedListener(new SimpleTextWatcher(e->item.name=e.toString()));
+			value.addTextChangedListener(new SimpleTextWatcher(e->item.value=e.toString()));
+			findViewById(R.id.remove_row_btn).setOnClickListener(this::onRemoveRowClick);
+		}
+
+		@Override
+		public void onBind(AccountField item){
+			title.setText(item.name);
+			value.setText(item.value);
+		}
+
+		private void onRemoveRowClick(View v){
+			int pos=getAbsoluteAdapterPosition();
+			metadataListData.remove(pos);
+			adapter.notifyItemRemoved(pos);
+			for(int i=0;i<list.getChildCount();i++){
+				BaseViewHolder vh=(BaseViewHolder) list.getChildViewHolder(list.getChildAt(i));
+				vh.rebind();
+			}
+		}
+	}
+
+	private class AddRowViewHolder extends BaseViewHolder implements UsableRecyclerView.Clickable{
+		public AddRowViewHolder(){
+			super(R.layout.item_profile_about_add_row);
+		}
+
+		@Override
+		public void onClick(){
+			metadataListData.add(new AccountField());
+			if(metadataListData.size()==MAX_FIELDS){ // replace this row with new row
+				adapter.notifyItemChanged(metadataListData.size()-1);
+			}else{
+				adapter.notifyItemInserted(metadataListData.size()-1);
+				rebind();
+			}
+		}
+
+		@Override
+		public void onBind(AccountField item) {}
+	}
+
+	private class ReorderCallback extends ItemTouchHelper.SimpleCallback{
+		public ReorderCallback(){
+			super(ItemTouchHelper.UP | ItemTouchHelper.DOWN, 0);
+		}
+
+		@Override
+		public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target){
+			if(target instanceof AddRowViewHolder)
+				return false;
+			int fromPosition=viewHolder.getAbsoluteAdapterPosition();
+			int toPosition=target.getAbsoluteAdapterPosition();
+			if (fromPosition<toPosition) {
+				for (int i=fromPosition;i<toPosition;i++) {
+					Collections.swap(metadataListData, i, i+1);
+				}
+			} else {
+				for (int i=fromPosition;i>toPosition;i--) {
+					Collections.swap(metadataListData, i, i-1);
+				}
+			}
+			adapter.notifyItemMoved(fromPosition, toPosition);
+			((BindableViewHolder)viewHolder).rebind();
+			((BindableViewHolder)target).rebind();
+			return true;
+		}
+
+		@Override
+		public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction){
+
+		}
+
+		@Override
+		public void onSelectedChanged(@Nullable RecyclerView.ViewHolder viewHolder, int actionState){
+			super.onSelectedChanged(viewHolder, actionState);
+			if(actionState==ItemTouchHelper.ACTION_STATE_DRAG){
+				viewHolder.itemView.setTag(R.id.item_touch_helper_previous_elevation, viewHolder.itemView.getElevation()); // prevents the default behavior of changing elevation in onDraw()
+				viewHolder.itemView.animate().translationZ(V.dp(1)).setDuration(200).setInterpolator(CubicBezierInterpolator.DEFAULT).start();
+				draggedViewHolder=viewHolder;
+			}
+		}
+
+		@Override
+		public void clearView(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder){
+			super.clearView(recyclerView, viewHolder);
+			viewHolder.itemView.animate().translationZ(0).setDuration(100).setInterpolator(CubicBezierInterpolator.DEFAULT).start();
+			draggedViewHolder=null;
+		}
+
+		@Override
+		public boolean isLongPressDragEnabled(){
+			return false;
 		}
 	}
 }
