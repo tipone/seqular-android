4152179dea54b28516a6e6fd6aa932febd223fbd
FineFindus <63370021+FineFindus@users.noreply.github.com>
2023-03-06 22:00:49 +0100

    fix(request/mute): add correct body on unmute

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/SetAccountMuted.java
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/SetAccountMuted.java
@@ -6,7 +6,7 @@ import org.joinmastodon.android.model.Relationship;
 public class SetAccountMuted extends MastodonAPIRequest<Relationship>{
 	public SetAccountMuted(String id, boolean muted, long duration){
 		super(HttpMethod.POST, "/accounts/"+id+"/"+(muted ? "mute" : "unmute"), Relationship.class);
-		setRequestBody(new Request(duration));
+		setRequestBody(muted ? new Request(duration): new Object());
 	}
 
 	private static class Request{
