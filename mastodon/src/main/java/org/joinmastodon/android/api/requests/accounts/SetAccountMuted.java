3619be71abf14763401222cd640a7bdbb4ce1f14
FineFindus <63370021+FineFindus@users.noreply.github.com>
2023-03-06 21:55:32 +0100

    feat: add mute timer

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/SetAccountMuted.java
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/SetAccountMuted.java
@@ -4,8 +4,15 @@ import org.joinmastodon.android.api.MastodonAPIRequest;
 import org.joinmastodon.android.model.Relationship;
 
 public class SetAccountMuted extends MastodonAPIRequest<Relationship>{
-	public SetAccountMuted(String id, boolean muted){
+	public SetAccountMuted(String id, boolean muted, long duration){
 		super(HttpMethod.POST, "/accounts/"+id+"/"+(muted ? "mute" : "unmute"), Relationship.class);
-		setRequestBody(new Object());
+		setRequestBody(new Request(duration));
+	}
+
+	private static class Request{
+		public long duration;
+		public Request(long duration){
+			this.duration=duration;
+		}
 	}
 }
