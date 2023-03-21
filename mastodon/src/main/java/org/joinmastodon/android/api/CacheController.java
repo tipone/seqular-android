ffb7cc5c181ce1c0cb70adbca8f83a349d10f40e
sk <sk22@mailbox.org>
2023-03-21 15:25:01 +0100

    don't remove badge if loaded from cache

--- mastodon/src/main/java/org/joinmastodon/android/api/CacheController.java
+++ mastodon/src/main/java/org/joinmastodon/android/api/CacheController.java
@@ -126,7 +126,7 @@ public class CacheController{
 		});
 	}
 
-	public void getNotifications(String maxID, int count, boolean onlyMentions, boolean onlyPosts, boolean forceReload, Callback<PaginatedResponse<List<Notification>>> callback){
+	public void getNotifications(String maxID, int count, boolean onlyMentions, boolean onlyPosts, boolean forceReload, Callback<CacheablePaginatedResponse<List<Notification>>> callback){
 		cancelDelayedClose();
 		databaseThread.postRunnable(()->{
 			try{
@@ -153,7 +153,7 @@ public class CacheController{
 								result.add(ntf);
 							}while(cursor.moveToNext());
 							String _newMaxID=newMaxID;
-							uiHandler.post(()->callback.onSuccess(new PaginatedResponse<>(result, _newMaxID)));
+							uiHandler.post(()->callback.onSuccess(new CacheablePaginatedResponse<>(result, _newMaxID, true)));
 							return;
 						}
 					}catch(IOException x){
@@ -164,7 +164,7 @@ public class CacheController{
 						.setCallback(new Callback<>(){
 							@Override
 							public void onSuccess(List<Notification> result){
-								callback.onSuccess(new PaginatedResponse<>(result.stream().filter(ntf->{
+								callback.onSuccess(new CacheablePaginatedResponse<>(result.stream().filter(ntf->{
 									if(ntf.status!=null){
 										for(Filter filter:filters){
 											if(filter.matches(ntf.status)){
@@ -173,7 +173,7 @@ public class CacheController{
 										}
 									}
 									return true;
-								}).collect(Collectors.toList()), result.isEmpty() ? null : result.get(result.size()-1).id));
+								}).collect(Collectors.toList()), result.isEmpty() ? null : result.get(result.size()-1).id, false));
 								putNotifications(result, onlyMentions, onlyPosts, maxID==null);
 							}
 
