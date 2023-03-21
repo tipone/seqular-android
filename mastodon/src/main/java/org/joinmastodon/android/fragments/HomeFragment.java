1680c635dc00234a82f42924cedfdf14e1d9f506
LucasGGamerM <lucassggabriel@gmail.com>
2023-03-21 19:47:51 -0300

    Revert "feat(tabs/notifications): add unread badge"
    This reverts commit 4ecd525f

--- mastodon/src/main/java/org/joinmastodon/android/fragments/HomeFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/HomeFragment.java
@@ -124,23 +124,6 @@ public class HomeFragment extends AppKitFragment implements OnBackPressedListene
 		Account self=AccountSessionManager.getInstance().getAccount(accountID).self;
 		ViewImageLoader.load(tabBarAvatar, null, new UrlImageLoaderRequest(self.avatar, V.dp(28), V.dp(28)));
 
-		notificationTabIcon=content.findViewById(R.id.tab_notifications);
-
-		AccountSessionManager.getInstance()
-				.getAccount(accountID).getCacheController()
-				.getNotifications(null, 1, false, false, true, new Callback<>() {
-					@Override
-					public void onSuccess(PaginatedResponse<List<Notification>> result) {
-						notificationBadged = result.items.get(0).createdAt.isAfter(Instant.ofEpochMilli(GlobalUserPreferences.lastNotificationOpenedTime));
-						setNotificationBadge();
-					}
-
-					@Override
-					public void onError(ErrorResponse error) {
-						error.showToast(getContext());
-					}
-				});
-
 		if(savedInstanceState==null){
 			getChildFragmentManager().beginTransaction()
 					.add(R.id.fragment_wrap, homeTabFragment)
@@ -367,9 +350,5 @@ public class HomeFragment extends AppKitFragment implements OnBackPressedListene
 //		getChildFragmentManager().putFragment(outState, "searchFragment", searchFragment);
 //		getChildFragmentManager().putFragment(outState, "notificationsFragment", notificationsFragment);
 //		getChildFragmentManager().putFragment(outState, "profileFragment", profileFragment);
-	}
-
-	private void setNotificationBadge() {
-			notificationTabIcon.setImageDrawable(getContext().getDrawable(notificationBadged ? R.drawable.ic_notifications_tab_badged : R.drawable.ic_fluent_alert_28_selector));
 	}
 }
