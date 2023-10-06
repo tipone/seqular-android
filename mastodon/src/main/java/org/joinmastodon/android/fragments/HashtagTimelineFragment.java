697f801c1a67969508134bbc637c94212eee5c3f
LucasGGamerM <71328265+LucasGGamerM@users.noreply.github.com>
2023-10-06 10:03:25 -0300

    fix(hashtags): fix crash when hashtag is null (#844)

--- mastodon/src/main/java/org/joinmastodon/android/fragments/HashtagTimelineFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/HashtagTimelineFragment.java
@@ -217,8 +217,6 @@ public class HashtagTimelineFragment extends PinnableStatusListFragment{
 		followMenuItem=optionsMenu.findItem(R.id.follow_hashtag);
 		pinMenuItem=optionsMenu.findItem(R.id.pin);
 		followMenuItem.setVisible(toolbarContentVisible);
-		followMenuItem.setTitle(getString(hashtag.following ? R.string.unfollow_user : R.string.follow_user, "#"+hashtagName));
-		followMenuItem.setIcon(hashtag.following ? R.drawable.ic_fluent_person_delete_24_filled : R.drawable.ic_fluent_person_add_24_regular);
 		pinMenuItem.setShowAsAction(toolbarContentVisible ? MenuItem.SHOW_AS_ACTION_NEVER : MenuItem.SHOW_AS_ACTION_ALWAYS);
 		super.updatePinButton(pinMenuItem);
 		if(toolbarContentVisible){
