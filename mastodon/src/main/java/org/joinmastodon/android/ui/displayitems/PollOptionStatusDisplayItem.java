efc67fd7e8818af6dad291d88c9f999eb3ff82d7
sk <sk22@mailbox.org>
2023-10-06 17:26:59 +0200

    remove inset poll styles
    closes sk22#801

--- mastodon/src/main/java/org/joinmastodon/android/ui/displayitems/PollOptionStatusDisplayItem.java
+++ mastodon/src/main/java/org/joinmastodon/android/ui/displayitems/PollOptionStatusDisplayItem.java
@@ -68,7 +68,7 @@ public class PollOptionStatusDisplayItem extends StatusDisplayItem{
 		private final TextView text, percent;
 		private final View button;
 		private final ImageView icon;
-		private final Drawable progressBg, progressBgInset;
+		private final Drawable progressBg;
 
 		public Holder(Activity activity, ViewGroup parent){
 			super(activity, R.layout.display_item_poll_option, parent);
@@ -77,7 +77,6 @@ public class PollOptionStatusDisplayItem extends StatusDisplayItem{
 			icon=findViewById(R.id.icon);
 			button=findViewById(R.id.button);
 			progressBg=activity.getResources().getDrawable(R.drawable.bg_poll_option_voted, activity.getTheme()).mutate();
-			progressBgInset=activity.getResources().getDrawable(R.drawable.bg_poll_option_voted_inset, activity.getTheme()).mutate();
 			itemView.setOnClickListener(this::onButtonClick);
 			button.setOutlineProvider(OutlineProviders.roundedRect(20));
 			button.setClipToOutline(true);
@@ -93,22 +92,17 @@ public class PollOptionStatusDisplayItem extends StatusDisplayItem{
 					item.showResults ? R.drawable.ic_poll_option_button : R.drawable.ic_fluent_radio_button_24_selector
 			));
 			if(item.showResults){
-				Drawable bg=item.inset ? progressBgInset : progressBg;
+				Drawable bg=progressBg;
 				bg.setLevel(Math.round(10000f*item.votesFraction));
 				button.setBackground(bg);
 				itemView.setSelected(item.poll.ownVotes!=null && item.poll.ownVotes.contains(item.optionIndex));
 				percent.setText(String.format(Locale.getDefault(), "%d%%", Math.round(item.votesFraction*100f)));
 			}else{
 				itemView.setSelected(item.poll.selectedOptions!=null && item.poll.selectedOptions.contains(item.option));
-				button.setBackgroundResource(item.inset ? R.drawable.bg_poll_option_clickable_inset : R.drawable.bg_poll_option_clickable);
+				button.setBackgroundResource(R.drawable.bg_poll_option_clickable);
 			}
-			if(item.inset){
-				text.setTextColor(itemView.getContext().getColorStateList(R.color.poll_option_text_inset));
-				percent.setTextColor(itemView.getContext().getColorStateList(R.color.poll_option_text_inset));
-			}else{
-				text.setTextColor(UiUtils.getThemeColor(itemView.getContext(), android.R.attr.textColorPrimary));
-				percent.setTextColor(UiUtils.getThemeColor(itemView.getContext(), R.attr.colorM3OnSecondaryContainer));
-			}
+			text.setTextColor(UiUtils.getThemeColor(itemView.getContext(), android.R.attr.textColorPrimary));
+			percent.setTextColor(UiUtils.getThemeColor(itemView.getContext(), R.attr.colorM3OnSecondaryContainer));
 		}
 
 		@Override
