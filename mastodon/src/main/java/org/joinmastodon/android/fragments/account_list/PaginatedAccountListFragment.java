tipone/seqular-android

commit 76867a971b5ff23e05e823620ce2d3ac5ccaa4f7

chore(merge-upstream): more conflict solving tomfoolery

mastodon/src/main/java/org/joinmastodon/android/fragments/account_list/PaginatedAccountListFragment.java
@@ -45,7 +45,8 @@ public abstract class PaginatedAccountListFragment<T> extends BaseAccountListFra
 		}
 
 		remoteDisabled = !GlobalUserPreferences.allowRemoteLoading
-				|| getSession().domain.equals(getRemoteDomain());
+				|| getSession().domain.equals(getRemoteDomain())
+				|| remoteInfoRequest == null;
 		if (!remoteDisabled) {
 			remoteInfoRequest = loadRemoteInfo().setCallback(new Callback<>() {
 				@Override

mastodon/src/main/java/org/joinmastodon/android/api/requests/lists/GetList.java
@@ -1,10 +1,10 @@
 package org.joinmastodon.android.api.requests.lists;
 
 import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.ListTimeline;
+import org.joinmastodon.android.model.FollowList;
 
-public class GetList extends MastodonAPIRequest<ListTimeline> {
+public class GetList extends MastodonAPIRequest<FollowList> {
 	public GetList(String id) {
-		super(HttpMethod.GET, "/lists/" + id, ListTimeline.class);
+		super(HttpMethod.GET, "/lists/" + id, FollowList.class);
 	}
 }

mastodon/src/main/java/org/joinmastodon/android/api/requests/lists/GetLists.java
@@ -4,7 +4,6 @@ import com.google.gson.reflect.TypeToken;
 
 import org.joinmastodon.android.api.MastodonAPIRequest;
 import org.joinmastodon.android.model.FollowList;
-import org.joinmastodon.android.model.ListTimeline;
 
 import java.util.List;
 

mastodon/src/main/java/org/joinmastodon/android/events/ListUpdatedCreatedEvent.java
@@ -1,14 +1,14 @@
 package org.joinmastodon.android.events;
 
-import org.joinmastodon.android.model.ListTimeline;
+import org.joinmastodon.android.model.FollowList;
 
 public class ListUpdatedCreatedEvent {
 	public final String id;
 	public final String title;
-	public final ListTimeline.RepliesPolicy repliesPolicy;
+	public final FollowList.RepliesPolicy repliesPolicy;
 	public final boolean exclusive;
 
-	public ListUpdatedCreatedEvent(String id, String title, boolean exclusive, ListTimeline.RepliesPolicy repliesPolicy) {
+	public ListUpdatedCreatedEvent(String id, String title, boolean exclusive, FollowList.RepliesPolicy repliesPolicy) {
 		this.id = id;
 		this.title = title;
 		this.exclusive = exclusive;

mastodon/src/main/java/org/joinmastodon/android/fragments/CreateListFragment.java
@@ -51,7 +51,7 @@ public class CreateListFragment extends BaseEditListFragment{
 
 	@Override
 	protected int getNavigationIconDrawableResource(){
-		return R.drawable.ic_baseline_close_24;
+		return R.drawable.ic_baseline_arrow_drop_down_18;
 	}
 
 	@Override

mastodon/src/main/java/org/joinmastodon/android/fragments/EditTimelinesFragment.java
@@ -41,17 +41,14 @@ import org.joinmastodon.android.api.requests.lists.GetLists;
 import org.joinmastodon.android.api.requests.tags.GetFollowedHashtags;
 import org.joinmastodon.android.api.session.AccountLocalPreferences;
 import org.joinmastodon.android.api.session.AccountSessionManager;
-import org.joinmastodon.android.api.session.AccountSession;
-import org.joinmastodon.android.api.session.AccountSessionManager;
 import org.joinmastodon.android.model.CustomLocalTimeline;
+import org.joinmastodon.android.model.FollowList;
 import org.joinmastodon.android.model.Hashtag;
 import org.joinmastodon.android.model.HeaderPaginationList;
-import org.joinmastodon.android.model.ListTimeline;
 import org.joinmastodon.android.model.TimelineDefinition;
 import org.joinmastodon.android.ui.DividerItemDecoration;
 import org.joinmastodon.android.ui.M3AlertDialogBuilder;
 import org.joinmastodon.android.ui.utils.UiUtils;
-import org.joinmastodon.android.ui.views.TextInputFrameLayout;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -74,7 +71,7 @@ public class EditTimelinesFragment extends MastodonRecyclerFragment<TimelineDefi
 	private Menu optionsMenu;
 	private boolean updated;
 	private final Map<MenuItem, TimelineDefinition> timelineByMenuItem=new HashMap<>();
-	private final List<ListTimeline> listTimelines=new ArrayList<>();
+	private final List<FollowList> followLists =new ArrayList<>();
 	private final List<Hashtag> hashtags=new ArrayList<>();
 	private MenuItem addHashtagItem;
     private final List<CustomLocalTimeline> localTimelines = new ArrayList<>();
@@ -94,8 +91,8 @@ public class EditTimelinesFragment extends MastodonRecyclerFragment<TimelineDefi
 
 		new GetLists().setCallback(new Callback<>(){
 			@Override
-			public void onSuccess(List<ListTimeline> result){
-				listTimelines.addAll(result);
+			public void onSuccess(List<FollowList> result){
+				followLists.addAll(result);
 				updateOptionsMenu();
 			}
 
@@ -224,7 +221,7 @@ public class EditTimelinesFragment extends MastodonRecyclerFragment<TimelineDefi
         makeBackItem(hashtagsMenu);
 
 		TimelineDefinition.getAllTimelines(accountID).stream().forEach(tl->addTimelineToOptions(tl, timelinesMenu));
-		listTimelines.stream().map(TimelineDefinition::ofList).forEach(tl->addTimelineToOptions(tl, listsMenu));
+		followLists.stream().map(TimelineDefinition::ofList).forEach(tl->addTimelineToOptions(tl, listsMenu));
 		addHashtagItem=addOptionsItem(hashtagsMenu, getContext().getString(R.string.sk_timelines_add), R.drawable.ic_fluent_add_24_regular);
 		hashtags.stream().map(TimelineDefinition::ofHashtag).forEach(tl->addTimelineToOptions(tl, hashtagsMenu));
 

mastodon/src/main/java/org/joinmastodon/android/fragments/HomeTabFragment.java
@@ -53,7 +53,7 @@ import org.joinmastodon.android.fragments.settings.SettingsMainFragment;
 import org.joinmastodon.android.model.Announcement;
 import org.joinmastodon.android.model.Hashtag;
 import org.joinmastodon.android.model.HeaderPaginationList;
-import org.joinmastodon.android.model.ListTimeline;
+import org.joinmastodon.android.model.FollowList;
 import org.joinmastodon.android.model.TimelineDefinition;
 import org.joinmastodon.android.ui.SimpleViewHolder;
 import org.joinmastodon.android.ui.utils.UiUtils;
@@ -95,7 +95,7 @@ public class HomeTabFragment extends MastodonToolbarFragment implements Scrollab
 	private ImageView collapsedChevron;
 	private TextView timelineTitle;
 	private PopupMenu switcherPopup;
-	private final Map<Integer, ListTimeline> listItems = new HashMap<>();
+	private final Map<Integer, FollowList> listItems = new HashMap<>();
 	private final Map<Integer, Hashtag> hashtagsItems = new HashMap<>();
 	private List<TimelineDefinition> timelinesList;
 	private int count;
@@ -270,7 +270,7 @@ public class HomeTabFragment extends MastodonToolbarFragment implements Scrollab
 
 		new GetLists().setCallback(new Callback<>() {
 			@Override
-			public void onSuccess(List<ListTimeline> lists) {
+			public void onSuccess(List<FollowList> lists) {
 				updateList(lists, listItems);
 			}
 
@@ -512,7 +512,7 @@ public class HomeTabFragment extends MastodonToolbarFragment implements Scrollab
 		Bundle args=new Bundle();
 		args.putString("account", accountID);
 		int id = item.getItemId();
-		ListTimeline list;
+		FollowList list;
 		Hashtag hashtag;
 
 		if (item.getItemId() == R.id.menu_back) {
@@ -701,13 +701,13 @@ public class HomeTabFragment extends MastodonToolbarFragment implements Scrollab
 
 	@Subscribe
 	public void onListDeletedEvent(ListDeletedEvent event) {
-		handleListEvent(listItems, l -> l.id.equals(event.id), false, null);
+		handleListEvent(listItems, l -> l.id.equals(event.listID), false, null);
 	}
 
 	@Subscribe
 	public void onListUpdatedCreatedEvent(ListUpdatedCreatedEvent event) {
 		handleListEvent(listItems, l -> l.id.equals(event.id), true, () -> {
-			ListTimeline list = new ListTimeline();
+			FollowList list = new FollowList();
 			list.id = event.id;
 			list.title = event.title;
 			list.repliesPolicy = event.repliesPolicy;

mastodon/src/main/java/org/joinmastodon/android/fragments/ListMembersFragment.java
@@ -1,5 +1,6 @@
 package org.joinmastodon.android.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.ActionMode;
 import android.view.Menu;
@@ -13,6 +14,7 @@ import com.squareup.otto.Subscribe;
 
 import org.joinmastodon.android.E;
 import org.joinmastodon.android.R;
+import org.joinmastodon.android.api.MastodonAPIRequest;
 import org.joinmastodon.android.api.requests.HeaderPaginationRequest;
 import org.joinmastodon.android.api.requests.lists.AddAccountsToList;
 import org.joinmastodon.android.api.requests.lists.GetListAccounts;
@@ -77,6 +79,21 @@ public class ListMembersFragment extends PaginatedAccountListFragment{
 	}
 
 	@Override
+	protected MastodonAPIRequest loadRemoteInfo(){
+		return null;
+	}
+
+	@Override
+	public Object getCurrentInfo(){
+		return null;
+	}
+
+	@Override
+	public String getRemoteDomain(){
+		return null;
+	}
+
+	@Override
 	protected void onConfigureViewHolder(AccountViewHolder holder){
 		super.onConfigureViewHolder(holder);
 		holder.setStyle(inSelectionMode ? AccountViewHolder.AccessoryType.CHECKBOX : AccountViewHolder.AccessoryType.MENU, false);
@@ -125,7 +142,7 @@ public class ListMembersFragment extends PaginatedAccountListFragment{
 	public void onViewCreated(View view, Bundle savedInstanceState){
 		super.onViewCreated(view, savedInstanceState);
 		fab=view.findViewById(R.id.fab);
-		fab.setImageResource(R.drawable.ic_add_24px);
+		fab.setImageResource(R.drawable.ic_fluent_add_24_regular);
 		fab.setContentDescription(getString(R.string.add_list_member));
 		fab.setOnClickListener(v->onFabClick());
 	}
@@ -297,5 +314,10 @@ public class ListMembersFragment extends PaginatedAccountListFragment{
 				list.getAdapter().notifyItemRemoved(i);
 			}
 		}
+	}
+
+	@Override
+	public Uri getWebUri(Uri.Builder base){
+		return null;
 	}
 }

commit a7b51095fb3a8f0c7b4afee5a0f2e9f180b5893e

fix(remote-info): remove null check
Fixes a regression introduced  in 76867a971b5ff23e05e823620ce2d3ac5ccaa4f7, that caused remote info to be permanently disabled

mastodon/src/main/java/org/joinmastodon/android/fragments/account_list/PaginatedAccountListFragment.java
@@ -45,8 +45,7 @@ public abstract class PaginatedAccountListFragment<T> extends BaseAccountListFra
 		}
 
 		remoteDisabled = !GlobalUserPreferences.allowRemoteLoading
-				|| getSession().domain.equals(getRemoteDomain())
-				|| remoteInfoRequest == null;
+				|| getSession().domain.equals(getRemoteDomain());
 		if (!remoteDisabled) {
 			remoteInfoRequest = loadRemoteInfo().setCallback(new Callback<>() {
 				@Override