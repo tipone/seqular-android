tipone/seqular-android

commit 0af8dbf09b7ca78f64842de479fdcb70ef6b86ac

chore(merging-upstream): bunch of conflicts to solve

mastodon/src/main/java/org/joinmastodon/android/ui/sheets/DecentralizationExplainerSheet.java
@@ -1,101 +1,0 @@
-package org.joinmastodon.android.ui.sheets;
-
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
-import android.graphics.drawable.ColorDrawable;
-import android.text.SpannableStringBuilder;
-import android.text.Spanned;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
-
-import org.joinmastodon.android.R;
-import org.joinmastodon.android.api.session.AccountSessionManager;
-import org.joinmastodon.android.model.Account;
-import org.joinmastodon.android.ui.M3AlertDialogBuilder;
-import org.joinmastodon.android.ui.Snackbar;
-import org.joinmastodon.android.ui.text.LinkSpan;
-import org.joinmastodon.android.ui.utils.UiUtils;
-import org.joinmastodon.android.ui.views.RippleAnimationTextView;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.nodes.TextNode;
-import org.jsoup.select.NodeVisitor;
-
-import androidx.annotation.NonNull;
-import me.grishka.appkit.views.BottomSheet;
-
-public class DecentralizationExplainerSheet extends BottomSheet{
-	private final String handleStr;
-
-	public DecentralizationExplainerSheet(@NonNull Context context, String accountID, Account account){
-		super(context);
-		View content=context.getSystemService(LayoutInflater.class).inflate(R.layout.sheet_decentralization_info, null);
-		setContentView(content);
-		setNavigationBarBackground(new ColorDrawable(UiUtils.alphaBlendColors(UiUtils.getThemeColor(context, R.attr.colorM3Surface),
-				UiUtils.getThemeColor(context, R.attr.colorM3Primary), 0.05f)), !UiUtils.isDarkTheme());
-
-		TextView handleTitle=findViewById(R.id.handle_title);
-		RippleAnimationTextView handle=findViewById(R.id.handle);
-		TextView usernameExplanation=findViewById(R.id.username_text);
-		TextView serverExplanation=findViewById(R.id.server_text);
-		TextView handleExplanation=findViewById(R.id.handle_explanation);
-		findViewById(R.id.btn_cancel).setOnClickListener(v->dismiss());
-
-		String domain=account.getDomain();
-		if(TextUtils.isEmpty(domain))
-			domain=AccountSessionManager.get(accountID).domain;
-		handleStr="@"+account.username+"@"+domain;
-		boolean isSelf=AccountSessionManager.getInstance().isSelf(accountID, account);
-
-		handleTitle.setText(isSelf ? R.string.handle_title_own : R.string.handle_title);
-		handle.setText(handleStr);
-		usernameExplanation.setText(isSelf ? R.string.handle_username_explanation_own : R.string.handle_username_explanation);
-		serverExplanation.setText(isSelf ? R.string.handle_server_explanation_own : R.string.handle_server_explanation);
-
-		String explanation=context.getString(isSelf ? R.string.handle_explanation_own : R.string.handle_explanation);
-		SpannableStringBuilder ssb=new SpannableStringBuilder();
-		Jsoup.parseBodyFragment(explanation).body().traverse(new NodeVisitor(){
-			private int spanStart;
-			@Override
-			public void head(Node node, int depth){
-				if(node instanceof TextNode tn){
-					ssb.append(tn.text());
-				}else if(node instanceof Element){
-					spanStart=ssb.length();
-				}
-			}
-
-			@Override
-			public void tail(Node node, int depth){
-				if(node instanceof Element){
-					ssb.setSpan(new LinkSpan("", DecentralizationExplainerSheet.this::showActivityPubAlert, LinkSpan.Type.CUSTOM, null, null, null), spanStart, ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-				}
-			}
-		});
-		handleExplanation.setText(ssb);
-
-		findViewById(R.id.handle_wrap).setOnClickListener(v->{
-			context.getSystemService(ClipboardManager.class).setPrimaryClip(ClipData.newPlainText(null, handleStr));
-			if(UiUtils.needShowClipboardToast()){
-				new Snackbar.Builder(context)
-						.setText(R.string.handle_copied)
-						.show();
-			}
-		});
-		String _domain=domain;
-		findViewById(R.id.username_row).setOnClickListener(v->handle.animate(1, account.username.length()+1));
-		findViewById(R.id.server_row).setOnClickListener(v->handle.animate(handleStr.length()-_domain.length(), handleStr.length()));
-	}
-
-	private void showActivityPubAlert(LinkSpan s){
-		new M3AlertDialogBuilder(getContext())
-				.setTitle(R.string.what_is_activitypub_title)
-				.setMessage(R.string.what_is_activitypub)
-				.setPositiveButton(R.string.ok, null)
-				.show();
-	}
-}

mastodon/src/main/java/org/joinmastodon/android/ui/text/HtmlParser.java
@@ -1,11 +1,22 @@
 package org.joinmastodon.android.ui.text;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.style.BackgroundColorSpan;
+import android.text.style.BulletSpan;
 import android.text.style.ForegroundColorSpan;
+import android.text.style.LeadingMarginSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StrikethroughSpan;
+import android.text.style.StyleSpan;
+import android.text.style.SubscriptSpan;
+import android.text.style.SuperscriptSpan;
+import android.text.style.TypefaceSpan;
+import android.text.style.UnderlineSpan;
 import android.widget.TextView;
 
 import com.twitter.twittertext.Regex;
@@ -26,6 +37,7 @@ import org.jsoup.safety.Safelist;
 import org.jsoup.select.NodeVisitor;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -34,6 +46,8 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import androidx.annotation.NonNull;
+
+import me.grishka.appkit.utils.V;
 
 public class HtmlParser{
 	private static final String TAG="HtmlParser";
@@ -57,6 +71,18 @@ public class HtmlParser{
 
 	private HtmlParser(){}
 
+	public static SpannableStringBuilder parse(String source, List<Emoji> emojis, List<Mention> mentions, List<Hashtag> tags, String accountID){
+		return parse(source, emojis, mentions, tags, accountID, null, null);
+	}
+
+	public static SpannableStringBuilder parse(String source, List<Emoji> emojis, List<Mention> mentions, List<Hashtag> tags, String accountID, Context context){
+		return parse(source, emojis, mentions, tags, accountID, null, context);
+	}
+
+	public static SpannableStringBuilder parse(String source, List<Emoji> emojis, List<Mention> mentions, List<Hashtag> tags, String accountID, Object parentObject){
+		return parse(source, emojis, mentions, tags, accountID, parentObject, null);
+	}
+
 	/**
 	 * Parse HTML and custom emoji into a spanned string for display.
 	 * Supported tags: <ul>
@@ -69,16 +95,22 @@ public class HtmlParser{
 	 * @param emojis Custom emojis that are present in source as <code>:code:</code>
 	 * @return a spanned string
 	 */
-	public static SpannableStringBuilder parse(String source, List<Emoji> emojis, List<Mention> mentions, List<Hashtag> tags, String accountID, Object parentObject){
+	public static SpannableStringBuilder parse(String source, List<Emoji> emojis, List<Mention> mentions, List<Hashtag> tags, String accountID, Object parentObject, Context context){
 		class SpanInfo{
 			public Object span;
 			public int start;
 			public Element element;
+			public boolean more;
 
 			public SpanInfo(Object span, int start, Element element){
+				this(span, start, element, false);
+			}
+
+			public SpanInfo(Object span, int start, Element element, boolean more){
 				this.span=span;
 				this.start=start;
 				this.element=element;
+				this.more=more;
 			}
 		}
 
@@ -88,6 +120,9 @@ public class HtmlParser{
 		Map<String, Hashtag> tagsByTag=tags.stream().distinct().collect(Collectors.toMap(t->t.name.toLowerCase(), Function.identity()));
 
 		final SpannableStringBuilder ssb=new SpannableStringBuilder();
+		int colorInsert=UiUtils.getThemeColor(context, R.attr.colorM3Success);
+		int colorDelete=UiUtils.getThemeColor(context, R.attr.colorM3Error);
+
 		Jsoup.parseBodyFragment(source).body().traverse(new NodeVisitor(){
 			private final ArrayList<SpanInfo> openSpans=new ArrayList<>();
 
@@ -129,24 +164,62 @@ public class HtmlParser{
 								openSpans.add(new SpanInfo(new InvisibleSpan(), ssb.length(), el));
 							}
 						}
+						case "li" -> openSpans.add(new SpanInfo(new BulletSpan(V.dp(8)), ssb.length(), el));
+						case "em", "i" -> openSpans.add(new SpanInfo(new StyleSpan(Typeface.ITALIC), ssb.length(), el));
+						case "h1", "h2", "h3", "h4", "h5", "h6" -> {
+							// increase line height above heading (multiplying the margin)
+							if (node.previousSibling()!=null) ssb.setSpan(new RelativeSizeSpan(2), ssb.length() - 1, ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+							if (!node.nodeName().equals("h1")) {
+								openSpans.add(new SpanInfo(new StyleSpan(Typeface.BOLD), ssb.length(), el));
+							}
+							openSpans.add(new SpanInfo(new RelativeSizeSpan(switch(node.nodeName()) {
+								case "h1" -> 1.5f;
+								case "h2" -> 1.25f;
+								case "h3" -> 1.125f;
+								default -> 1;
+							}), ssb.length(), el, !node.nodeName().equals("h1")));
+						}
+						case "strong", "b" -> openSpans.add(new SpanInfo(new StyleSpan(Typeface.BOLD), ssb.length(), el));
+						case "u" -> openSpans.add(new SpanInfo(new UnderlineSpan(), ssb.length(), el));
+						case "s", "del" -> openSpans.add(new SpanInfo(new StrikethroughSpan(), ssb.length(), el));
+						case "sub", "sup" -> {
+							openSpans.add(new SpanInfo(node.nodeName().equals("sub") ? new SubscriptSpan() : new SuperscriptSpan(), ssb.length(), el));
+							openSpans.add(new SpanInfo(new RelativeSizeSpan(0.8f), ssb.length(), el, true));
+						}
+						case "code", "pre" -> openSpans.add(new SpanInfo(new TypefaceSpan("monospace"), ssb.length(), el));
+						case "blockquote" -> openSpans.add(new SpanInfo(new LeadingMarginSpan.Standard(V.dp(10)), ssb.length(), el));
+						// fake elements for the edit history diff view
+						case "edit-diff-insert" -> openSpans.add(new SpanInfo(new ForegroundColorSpan(colorInsert), ssb.length(), el));
+						case "edit-diff-delete" -> openSpans.add(new SpanInfo(new DiffRemovedSpan(el.text(), colorDelete), ssb.length(), el));
 					}
 				}
 			}
 
+			final static List<String> blockElements = Arrays.asList("p", "ul", "ol", "blockquote", "h1", "h2", "h3", "h4", "h5", "h6");
+
 			@Override
 			public void tail(@NonNull Node node, int depth){
 				if(node instanceof Element el){
+					processOpenSpan(el);
 					if("span".equals(el.nodeName()) && el.hasClass("ellipsis")){
 						ssb.append("…", new DeleteWhenCopiedSpan(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-					}else if("p".equals(el.nodeName())){
-						if(node.nextSibling()!=null)
-							ssb.append("\n\n");
-					}else if(!openSpans.isEmpty()){
-						SpanInfo si=openSpans.get(openSpans.size()-1);
-						if(si.element==el){
-							ssb.setSpan(si.span, si.start, ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-							openSpans.remove(openSpans.size()-1);
-						}
+					}else if(blockElements.contains(el.nodeName()) && node.nextSibling()!=null){
+						ssb.append("\n"); // line end
+						ssb.append("\n", new RelativeSizeSpan(0.65f), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE); // margin after block
+					}
+				}
+			}
+
+			private void processOpenSpan(Element el) {
+				if(!openSpans.isEmpty()){
+					SpanInfo si=openSpans.get(openSpans.size()-1);
+					if(si.element==el){
+						ssb.setSpan(si.span, si.start, ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+						openSpans.remove(openSpans.size()-1);
+						if(si.more) processOpenSpan(el);
+					}
+					if("li".equals(el.nodeName()) && el.nextSibling()!=null) {
+						ssb.append('\n');
 					}
 				}
 			}
@@ -157,6 +230,7 @@ public class HtmlParser{
 	}
 
 	public static void parseCustomEmoji(SpannableStringBuilder ssb, List<Emoji> emojis){
+		if(emojis==null) return;
 		Map<String, Emoji> emojiByCode =
 			emojis.stream()
 			.collect(
@@ -228,6 +302,10 @@ public class HtmlParser{
 		return sb.toString();
 	}
 
+	public static String text(String html) {
+		return Jsoup.parse(html).body().wholeText();
+	}
+
 	public static CharSequence parseLinks(String text){
 		Matcher matcher=URL_PATTERN.matcher(text);
 		if(!matcher.find()) // Return the original string if there are no URLs
@@ -243,6 +321,7 @@ public class HtmlParser{
 	}
 
 	public static void applyFilterHighlights(Context context, SpannableStringBuilder text, List<FilterResult> filters){
+		if (filters == null) return;
 		int fgColor=UiUtils.getThemeColor(context, R.attr.colorM3Error);
 		int bgColor=UiUtils.getThemeColor(context, R.attr.colorM3ErrorContainer);
 		for(FilterResult filter:filters){

mastodon/src/main/java/org/joinmastodon/android/ui/text/LinkSpan.java
@@ -3,7 +3,9 @@ package org.joinmastodon.android.ui.text;
 import android.content.Context;
 import android.text.TextPaint;
 import android.text.style.CharacterStyle;
+import android.view.View;
 
+import org.joinmastodon.android.GlobalUserPreferences;
 import org.joinmastodon.android.model.Hashtag;
 import org.joinmastodon.android.ui.utils.UiUtils;
 
@@ -33,7 +35,7 @@ public class LinkSpan extends CharacterStyle {
 	@Override
 	public void updateDrawState(TextPaint tp) {
 		tp.setColor(color=tp.linkColor);
-		tp.setUnderlineText(true);
+		tp.setUnderlineText(GlobalUserPreferences.underlinedLinks);
 	}
 	
 	public void onClick(Context context){
@@ -50,8 +52,19 @@ public class LinkSpan extends CharacterStyle {
 		}
 	}
 
+	public void onLongClick(View view) {
+		if(linkObject instanceof Hashtag ht)
+			UiUtils.copyText(view, ht.name);
+		else
+			UiUtils.copyText(view, link);
+	}
+
 	public String getLink(){
 		return link;
+	}
+
+	public String getText() {
+		return parentObject.toString();
 	}
 
 	public Type getType(){

commit c73fa2dd6bc80014738881817e03272261354f31

fix: use correct hashtag text
Fixes a regression introduced in 0af8dbf09b7ca78f64842de479fdcb70ef6b86ac, taht caused the app to crash when long pressing on a hastag to copy it

mastodon/src/main/java/org/joinmastodon/android/fragments/onboarding/SignupFragment.java
@@ -302,7 +302,7 @@ public class SignupFragment extends ToolbarFragment{
 						@Override
 						public void tail(Node node, int depth){
 							if(node instanceof Element){
-								ssb.setSpan(new LinkSpan("", SignupFragment.this::onGoBackLinkClick, LinkSpan.Type.CUSTOM, null, null, null), spanStart, ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+								ssb.setSpan(new LinkSpan("", SignupFragment.this::onGoBackLinkClick, LinkSpan.Type.CUSTOM, null, null, null, null), spanStart, ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 								ssb.setSpan(new TypefaceSpan("sans-serif-medium"), spanStart, ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 							}
 						}

mastodon/src/main/java/org/joinmastodon/android/ui/sheets/DecentralizationExplainerSheet.java
@@ -72,7 +72,7 @@ public class DecentralizationExplainerSheet extends BottomSheet{
 			@Override
 			public void tail(Node node, int depth){
 				if(node instanceof Element){
-					ssb.setSpan(new LinkSpan("", DecentralizationExplainerSheet.this::showActivityPubAlert, LinkSpan.Type.CUSTOM, null, null, null), spanStart, ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+					ssb.setSpan(new LinkSpan("", DecentralizationExplainerSheet.this::showActivityPubAlert, LinkSpan.Type.CUSTOM, null, null, null, null), spanStart, ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 				}
 			}
 		});

mastodon/src/main/java/org/joinmastodon/android/ui/text/HtmlParser.java
@@ -136,8 +136,8 @@ public class HtmlParser{
 							Object linkObject=null;
 							String href=el.attr("href");
 							LinkSpan.Type linkType;
+							String text=el.text();
 							if(el.hasClass("hashtag")){
-								String text=el.text();
 								if(text.startsWith("#")){
 									linkType=LinkSpan.Type.HASHTAG;
 									href=text.substring(1);
@@ -156,7 +156,7 @@ public class HtmlParser{
 							}else{
 								linkType=LinkSpan.Type.URL;
 							}
-							openSpans.add(new SpanInfo(new LinkSpan(href, null, linkType, accountID, linkObject, parentObject), ssb.length(), el));
+							openSpans.add(new SpanInfo(new LinkSpan(href, null, linkType, accountID, linkObject, parentObject, text), ssb.length(), el));
 						}
 						case "br" -> ssb.append('\n');
 						case "span" -> {
@@ -315,7 +315,7 @@ public class HtmlParser{
 			String url=matcher.group(3);
 			if(TextUtils.isEmpty(matcher.group(4)))
 				url="http://"+url;
-			ssb.setSpan(new LinkSpan(url, null, LinkSpan.Type.URL, null, null, null), matcher.start(3), matcher.end(3), 0);
+			ssb.setSpan(new LinkSpan(url, null, LinkSpan.Type.URL, null, null, null, url), matcher.start(3), matcher.end(3), 0);
 		}while(matcher.find()); // Find more URLs
 		return ssb;
 	}

mastodon/src/main/java/org/joinmastodon/android/ui/text/LinkSpan.java
@@ -18,14 +18,16 @@ public class LinkSpan extends CharacterStyle {
 	private String accountID;
 	private Object linkObject;
 	private Object parentObject;
+	private String text;
 
-	public LinkSpan(String link, OnLinkClickListener listener, Type type, String accountID, Object linkObject, Object parentObject){
+	public LinkSpan(String link, OnLinkClickListener listener, Type type, String accountID, Object linkObject, Object parentObject, String text){
 		this.listener=listener;
 		this.link=link;
 		this.type=type;
 		this.accountID=accountID;
 		this.linkObject=linkObject;
 		this.parentObject=parentObject;
+		this.text=text;
 	}
 
 	public int getColor(){
@@ -64,7 +66,7 @@ public class LinkSpan extends CharacterStyle {
 	}
 
 	public String getText() {
-		return parentObject.toString();
+		return text;
 	}
 
 	public Type getType(){