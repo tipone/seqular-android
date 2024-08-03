tipone/seqular-android

commit 5edbe9b82666e17c0f103678da6209ef7e9eaf17

fix(discover-fragment): put Posts fragment in the first place
Fixes #472

mastodon/src/main/java/org/joinmastodon/android/fragments/discover/DiscoverFragment.java
@@ -82,8 +82,8 @@ public class DiscoverFragment extends AppKitFragment implements ScrollableToTop,
 		for(int i=0;i<tabViews.length;i++){
 			FrameLayout tabView=new FrameLayout(getActivity());
 			tabView.setId(switch(i){
-				case 0 -> R.id.discover_hashtags;
-				case 1 -> R.id.discover_posts;
+				case 0 -> R.id.discover_posts;
+				case 1 -> R.id.discover_hashtags;
 				case 2 -> R.id.discover_news;
 				case 3 -> R.id.discover_users;
 				default -> throw new IllegalStateException("Unexpected value: "+i);
@@ -127,8 +127,8 @@ public class DiscoverFragment extends AppKitFragment implements ScrollableToTop,
 			accountsFragment.setArguments(args);
 
 			getChildFragmentManager().beginTransaction()
-					.add(R.id.discover_hashtags, hashtagsFragment)
 					.add(R.id.discover_posts, postsFragment)
+					.add(R.id.discover_hashtags, hashtagsFragment)
 					.add(R.id.discover_news, newsFragment)
 					.add(R.id.discover_users, accountsFragment)
 					.commit();
@@ -138,8 +138,8 @@ public class DiscoverFragment extends AppKitFragment implements ScrollableToTop,
 			@Override
 			public void onConfigureTab(@NonNull TabLayout.Tab tab, int position){
 				tab.setText(switch(position){
-					case 0 -> R.string.hashtags;
-					case 1 -> R.string.posts;
+					case 0 -> R.string.posts;
+					case 1 -> R.string.hashtags;
 					case 2 -> R.string.news;
 					case 3 -> R.string.for_you;
 					default -> throw new IllegalStateException("Unexpected value: "+position);

commit 2dada69eb8df51e3a2835771f7f483961b1c35a4

fix(Discover): switch post and hashtag fragments everywhere Fixes a regression in 5edbe9b82666e17c0f103678da6209ef7e9eaf17, whcih did not switch the fragments everywhere. This caused the scroll-to-top functionality to not work and the posts to not immediatly load.
Closes https://github.com/LucasGGamerM/moshidon/issues/483.

mastodon/src/main/java/org/joinmastodon/android/fragments/discover/DiscoverFragment.java
@@ -260,8 +260,8 @@ public class DiscoverFragment extends AppKitFragment implements ScrollableToTop,
 
 	private Fragment getFragmentForPage(int page){
 		return switch(page){
-			case 0 -> hashtagsFragment;
-			case 1 -> postsFragment;
+			case 0 -> postsFragment;
+			case 1 -> hashtagsFragment;
 			case 2 -> newsFragment;
 			case 3 -> accountsFragment;
 			default -> throw new IllegalStateException("Unexpected value: "+page);